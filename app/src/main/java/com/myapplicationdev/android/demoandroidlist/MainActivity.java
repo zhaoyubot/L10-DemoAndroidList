425b1307bf91fcf8eb12baa18097be76a673a11d
zhaoyubot <20011119@myrp.edu.sg>
2022-07-21 14:10:37 +0800

    Lesson 10

--- app/src/main/java/com/myapplicationdev/android/demoandroidlist/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoandroidlist/MainActivity.java
@@ -11,8 +11,11 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ListView lvAndroidVersions;
-    ArrayList<String> alAndroidVersions;
-    ArrayAdapter<String> aaAndroidVersions;
+    ArrayList<AndroidVersion> alAndroidVersions;
+
+    //ArrayAdapter<AndroidVersion> aaAndroidVersions;
+
+    CustomArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +25,25 @@ public class MainActivity extends AppCompatActivity {
         lvAndroidVersions = findViewById(R.id.listViewAndroidVersions);
         alAndroidVersions = new ArrayList<>();
 
-        alAndroidVersions.add("Pie - 9.0");
-        alAndroidVersions.add("Oreo - 8.0 - 8.1");
-        alAndroidVersions.add("Nougat - 7.0 - 7.1.2");
-        alAndroidVersions.add("Marshmallow - 6.0 - 6.0.1");
-        alAndroidVersions.add("Lollipop - 5.0 - 5.1.1");
-        alAndroidVersions.add("KitKat - 4.4 - 4.4.4");
-        alAndroidVersions.add("Jelly Bean - 4.1 - 4.3.1");
+        AndroidVersion item1 = new AndroidVersion("Pie", "9.0");
+        AndroidVersion item2 = new AndroidVersion("Oreo", "8.0 - 8.1");
+        AndroidVersion item3 = new AndroidVersion("Nougat", "7.0 - 7.1.2");
 
-        aaAndroidVersions = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, alAndroidVersions);
+        alAndroidVersions.add(item1);
+        alAndroidVersions.add(item2);
+        alAndroidVersions.add(item3);
 
-        lvAndroidVersions.setAdapter(aaAndroidVersions);
+
+        //aaAndroidVersions = new ArrayAdapter<>(this,,
+        //android.R.layout.simple_list_item_1, alAndroidVersions);
+
+
+        adapter = new CustomArrayAdapter(this, R.layout.row, alAndroidVersions);
+
+
+        //lvAndroidVersions.setAdapter(aaAndroidVersions);
+
+
+        lvAndroidVersions.setAdapter(adapter);
     }
-
 }
